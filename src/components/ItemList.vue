5e99f3781eddfb6ea5ca8f221ad1c51fb9a479c6
Amir Salehi <amirsalehi@Amirs-MacBook-Pro.local>
2024-08-06 08:53:00 -0700

    ui added

--- src/components/ItemList.vue
+++ src/components/ItemList.vue
@@ -1,5 +1,22 @@
 <template>
-    <div>
+    <div class="item-list">
         <h2>Todo List</h2>
+        <!-- Form to add new item -->
+        <form @submit.prevent="addNewItem">
+            <input v-model="newItemTitle" placeholder="Enter new item name" required>
+            <button type="submit">Add Item</button>
+        </form>
+
+        <!-- Loading and error messages -->
+        <p v-if="loading">Loading items...</p>
+        <p v-if="error">{{ error }}</p>
+
+        <!-- List of items -->
+        <transition-group name="list" tag="ul">
+            <li v-for="item in items" :key="item.id">
+                <p>{{ item.title }}</p>
+                <button @click="removeItem(item.id)">Remove</button>
+            </li>
+        </transition-group>
     </div>
 </template>
